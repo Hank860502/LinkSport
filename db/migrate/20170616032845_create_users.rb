6713ac015f8464eed94399112387b7f18fccffcd
Hank860502 <hank860502@gmail.com>
2017-06-28 15:47:19 +0800

    add new columns to user model, adding new gem carrierwave to let user upload image

--- db/migrate/20170616032845_create_users.rb
+++ db/migrate/20170616032845_create_users.rb
@@ -8,6 +8,17 @@ class CreateUsers < ActiveRecord::Migration
       t.string :image, :default_url => "/avatar.png"
       t.string :password_digest
       t.boolean :admin, default: false
+      t.integer :itf
+      t.integer :utr
+      t.integer :ntr
+      t.boolean :righthanded, default: true
+      t.boolean :double_handed_backhand, default: true
+      t.decimal :height
+      t.decimal :weight
+      t.integer :sat
+      t.integer :toefl
+      t.integer :act
+      t.date :birthday
 
       t.timestamps null: false
     end
